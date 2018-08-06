93afb13ad09017e89d04d60c7a1f78608f21c7a4
levont <lstonoyan@gmail.com>
2018-08-06 13:25:54 +0400

    changes are made doing development

--- src/main/java/com/example/demo/repository/RouteRepository.java
+++ src/main/java/com/example/demo/repository/RouteRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by levont on 7/26/2018.
@@ -13,8 +12,6 @@ import java.util.List;
 public interface RouteRepository extends CrudRepository<Record, Integer> {
 
     Record save(Record s);
-
-    List<Record> getAllByExpirationDateBefore(Date expirationDate);
 
     @Transactional
     Integer deleteAllByExpirationDateBefore(Date expirationDate);
